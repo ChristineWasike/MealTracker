67f0830dadef26cca20297e6a62f0cb4a43b2456
Christine Wasike <christinewasike22@gmail.com>
2017-07-07 12:25:17 +0300

    I have added and modified my app.module, completeness,main, meal,model, meals-list, bower.json, package-json, resources, typing files

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import{ Component } from '@angular/core';
+
+@Component ({
+  selector: "food",
+  template: `
+<div class="container">
+<h1>Your Meal Tracker</h1>
+
+</div>
+  `
+})
+
+export class AppComponent {
+
+}
---
+++ app/meal.model.ts
@@ -0,0 +1,4 @@
+export class Meal {
+  public full: boolean = true;
+  constructor(public name: string, public details: number, public calories: number) { }
+}
